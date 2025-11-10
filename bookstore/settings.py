0c3b3f35c39bac5036effe8cf3b93c41f3fc3392
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-11-10 10:18:50 +0000

    Added Bootstrap and Crispy Forms

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'books',                    # NEW
+    'books',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
