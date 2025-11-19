b4002f82af3440d4eb1cb5f3b1b4dee932d1422e
Shahzad Qadir <shahzadqadir@hotmail.co.uk>
2025-11-19 17:48:27 +0000

    Completed proof reading of Chapter 14 - Bootstrap and Crispy Forms

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -40,7 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
